8129bfed763030aeb64493a01e97d41034a72525
Hilalhidayah17 <hilalhidayah589456@gmmail.com>
2024-01-20 16:42:02 +0700

    menambahkan kode untuk menghilangkan navbar tanpa harus mengklik ikon humberger

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active"); //saat menu hamburger nya di klik maka class navbar-nav di toggle(kalau ada classnya ditambahin kalau tidak ada hilangin) akan menambahkan kelas active
 };
+
+//klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
